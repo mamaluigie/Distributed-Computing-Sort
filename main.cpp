1d450ef20f987447268b2f1fb1392aa8e305286b
mamaluigie <kane.hollingsworth@yahoo.com>
2020-05-15 21:48:17 -0500

    first hello world program in concurrency

--- main.cpp
+++ main.cpp
@@ -1,28 +1,15 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <thread>
 
 using namespace std;
 
+void hello()
+{
+    cout<<"Hello Concurrent World\n";
+}
+
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    
-    string x;
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
-
-    cin >> x;
-    #include <thread>
-
-    //may return 0 when not able to detect
-    const auto processor_count = thread::hardware_concurrency();
-
-    cout << "Processor Count = " << processor_count;
-    cin >> x;
+    thread t(hello);
+    t.join();
 }
